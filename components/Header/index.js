3e00baf7421d9ee3fa81130377f24995da61f1a5
Aaron-Doran <wesley.gossett@gmail.com>
2019-11-01 12:43:01 -0400

    init commit. Started on header index.js got stuck moved to tabs index,js then came back to header index,js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+function Header() {
+    const Wrapper = document.querySelector('.header-container')
+
+    const Card = document.createElement('div');
+    Card.classList.add('header');
+
+    const TimeStamp = document.createElement('span');
+    TimeStamp.textContent = text;
+    TimeStamp.classList.add('date')
+
+    const Title = document.createElement('h1');
+    Title.textContent = text;
+
+    const Temp = document.createElement('span');
+    Temp.textContent = text;
+    Temp.classList.add('temp');
+
+    Wrapper.appendChild(Card());
+    Card.appendChild(TimeStamp);
+    Card.appendChild(Title);
+    Card.appendChild(Temp);
+
+    console.log(Card());
+    return Card();
+}
